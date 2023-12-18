cb99bed90aef528996725765885748ca24a79f80
ashiq <rahman15-14610@diu.edu.bd>
2023-12-19 04:40:55 +0600

    Find and fix iteration error in string_occurrence.py and remove hash complexity

--- Module-3_String/string_occurrence.py
+++ Module-3_String/string_occurrence.py
@@ -4,16 +4,15 @@ def occurrence(haystack, needle):
         Given two strings needle and haystack, return the index of the first occurrence of needle in haystack,
         or -1 if needle is not part of haystack. Also, mention the Time and Space Complexity of your solution
 
-        Time complexity: O(log n)
+        Time complexity: O(n)
         Space complexity: O(1)
     """
-    needle_hash = hash(needle)
     needle_len = len(needle)
-    for i in range(0,len(haystack), needle_len):
-        current_hash = hash(haystack[i:needle_len])
-        if needle_hash == current_hash:
+    for i in range(len(haystack) - needle_len + 1):
+        if haystack[i:i + needle_len] == needle:
             return i
-        else: return -1
+    return -1
 
-print(occurrence(haystack = "sadbutsad", needle = "sad"))
-print(occurrence(haystack = "codemama", needle = "ostad"))
+
+print(occurrence(haystack="sadbutsad", needle="sad"))
+print(occurrence(haystack="codemama", needle="ostad"))
